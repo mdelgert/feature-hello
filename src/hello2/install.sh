661c6da7899b5bed0e11fe40e8bd5c5cd61c6f94
Matthew Elgert <mdelgert@yahoo.com>
2025-06-01 05:31:55 +0000

    Add hello feature implementation with install script and configuration

---
+++ src/hello2/install.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+set -e
+
+echo "Activating feature 'hello'"
+
+FEATURE_VERSION=${FEATUREVERSION:-"unknown"}
+
+echo "Installing Version: $FEATURE_VERSION"
+
+GREETING=${GREETING:-undefined}
+echo "The provided greeting is: $GREETING"
+
+# The 'install.sh' entrypoint script is always executed as the root user.
+#
+# These following environment variables are passed in by the dev container CLI.
+# These may be useful in instances where the context of the final 
+# remoteUser or containerUser is useful.
+# For more details, see https://containers.dev/implementors/features#user-env-var
+echo "The effective dev container remoteUser is '$_REMOTE_USER'"
+echo "The effective dev container remoteUser's home directory is '$_REMOTE_USER_HOME'"
+
+echo "The effective dev container containerUser is '$_CONTAINER_USER'"
+echo "The effective dev container containerUser's home directory is '$_CONTAINER_USER_HOME'"
+
+cat > /usr/local/bin/hello2 \
+<< EOF
+#!/bin/sh
+RED='\033[0;91m'
+NC='\033[0m' # No Color
+echo "\${RED}${GREETING}, \$(whoami)!\${NC}"
+echo "Version: ${FEATURE_VERSION}"
+EOF
+
+chmod +x /usr/local/bin/hello2
