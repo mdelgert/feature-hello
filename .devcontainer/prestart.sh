2c748d282b2a119365a03243fa5e01cce74f3592
Matthew Elgert <mdelgert@yahoo.com>
2025-06-01 04:34:26 +0000

    Add prestart script and update devcontainer configuration

---
+++ .devcontainer/prestart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+
+echo "Starting prestart script..."
+
+# touch ~/prestart.test
+
+# Copy your shared feature into the .devcontainer/features/ folder
+# mkdir -p .devcontainer/features
+# cp -r ../src/my-feature .devcontainer/features/my-feature
